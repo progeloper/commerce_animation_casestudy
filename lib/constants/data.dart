f862646f13d0a5bca9012c81d45f15445420fc42
Mayeavx <mayoodukoya@gmail.com>
2023-08-23 12:33:39 +0100

    initial commit

--- lib/constants/data.dart
+++ lib/constants/data.dart
@@ -1,10 +1,24 @@
+import 'package:flutter/material.dart';
+
 class Data{
-  static List<ProductModel> products = [
+  static List<Map<String, dynamic>> products = [
     {
-      'title': ,
-      'price': this._price,
-      'color': this._color,
-      'asset': this._asset,
+      'title': 'Air Jordans',
+      'price': 57999.99,
+      'color': Colors.grey,
+      'asset': 'assets/air_jordans_retro.obj',
+    },
+    {
+      'title': 'Nike Sail',
+      'price': 35000,
+      'color': Colors.blueGrey[100],
+      'asset': 'assets/nike_sail.stl',
+    },
+    {
+      'title': 'NikexSupreme Uptempo',
+      'price': 89999.99,
+      'color': Colors.red[300],
+      'asset': 'assets/nike_supreme.stl',
     },
   ];
 }
---
+++ lib/screens/select_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SelectScreen extends StatefulWidget {
+  const SelectScreen({super.key});
+
+  @override
+  State<SelectScreen> createState() => _SelectScreenState();
+}
+
+class _SelectScreenState extends State<SelectScreen> {
+
+  List<Widget> get _c
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
